8cf4685dadffbaec164f5ce84a85ff970bc4ae45
Jack Morris <jack.morris10@gmail.com>
2013-12-09 16:04:17 +0000

    Added quicksort implementation: Sorting/quicksort.py
    Follows standard defined in Sorting/SORTING-README.md. min_value and max_value parameters not required as sort is comparative.

---
+++ Sorting/quicksort.py
@@ -0,0 +1,37 @@
+""" Quicksort
+
+Time complexity: O(n^2)
+Space complexity: O(n) total, O(1) auxiliary (in place)
+
+"""
+
+
+def _partition(data, start_index, end_index):
+    """ Partitions data into two sections, less than partition value and greater than partition value. Partition
+    value chosen as last element in data. Partition value moved between sections in data, and its index returned.
+    """
+    partition_value = data[end_index-1]
+    i = start_index - 1
+    for j in range(start_index, end_index-1):
+        if data[j] <= partition_value:
+            i += 1
+            tmp = data[i]
+            data[i] = data[j]
+            data[j] = tmp
+    tmp = data[i+1]
+    data[i+1] = data[end_index-1]
+    data[end_index-1] = tmp
+    return i+1
+
+
+def _quicksort(data, start_index, end_index):
+    """ Called by quicksort so the sort can be done in place """
+    if start_index >= end_index:
+        return
+    partition_index = _partition(data, start_index, end_index)
+    _quicksort(data, start_index, partition_index)
+    _quicksort(data, partition_index+1, end_index)
+
+
+def quicksort(data, min_value=0, max_value=0):
+    _quicksort(data, 0, len(data))
