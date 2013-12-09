e5420ed8a858fa60546d44559a5bae2eefc58f75
Jack Morris <jack.morris10@gmail.com>
2013-12-09 21:42:19 +0000

    Added time complexity information to stack.py
    Future data structure implementations will also include this information.

--- DataStructures/stack.py
+++ DataStructures/stack.py
@@ -2,19 +2,19 @@ class Stack:
     """ Standard First-In, Last-Out stack implementation """
 
     def __init__(self):
-        """ Initialises an empty stack """
+        """ Initialises an empty stack. Complexity: O(1) """
         self.data = []
 
     def is_empty(self):
-        """ Returns True if the stack contains no elements """
+        """ Returns True if the stack contains no elements. Complexity: O(1) """
         return len(self.data) == 0
 
     def push(self, x):
-        """ Pushes an element x onto the stack """
+        """ Pushes an element x onto the stack. Complexity: O(1) """
         self.data.append(x)
 
     def pop(self):
-        """ Returns and removes the element at the head of the stack. If stack is empty, raises an IndexError """
+        """ Returns and removes the element at the head of the stack. Raises IndexError if empty. Complexity: O(1) """
         if self.is_empty():
             raise IndexError("stack empty")
         else:
