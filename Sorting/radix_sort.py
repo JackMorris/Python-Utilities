b8923e032646c402513ea4f60ad60081d38f8c8f
Jack Morris <jack.morris10@gmail.com>
2013-12-09 17:01:16 +0000

    Added radix sort implementation: Sorting/radix_sort.py
    Follows standard defined in Sorting/SORTING-README.md.

---
+++ Sorting/radix_sort.py
@@ -0,0 +1,25 @@
+""" Radix Sort
+
+Time complexity: O(kn) (k = digits_in_max)
+Space complexity: O(n)
+Note: only valid when all values in data > 0
+
+"""
+
+
+def radix_sort(data):
+    max_value = max(data)
+    digits_in_max = len(str(max_value))
+    for i in range(0, digits_in_max):
+        bins = []
+        for j in range(0, 10):
+            bins.append([])
+        div_power = 10**i
+        for v in data:
+            sort_digit = int(v/div_power) % 10
+            bins[sort_digit].append(v)
+        data_index = 0
+        for b in bins:
+            for v in b:
+                data[data_index] = v
+                data_index += 1
