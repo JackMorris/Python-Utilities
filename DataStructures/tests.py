ab20a6702f66f7dff59786e04145182b3c76d2df
Jack Morris <jack.morris10@gmail.com>
2013-12-09 21:58:49 +0000

    Added data structure unit tests.
    DataStructures/tests.py now contains tests for stack.py. Further tests will be added for each new data structure implementation.

---
+++ DataStructures/tests.py
@@ -0,0 +1,48 @@
+from stack import *
+import unittest
+import random
+
+
+class TestStack(unittest.TestCase):
+    def setUp(self):
+        self.s = Stack()
+
+    def test_empty_stack(self):
+        self.assertTrue(self.s.is_empty())
+
+    def test_single_item_non_empty(self):
+        self.s.push(random.randint(-1000000, 1000000))
+        self.assertFalse(self.s.is_empty())
+
+    def test_single_item_push_pop_empty(self):
+        self.s.push(random.randint(-1000000, 1000000))
+        self.s.pop()
+        self.assertTrue(self.s.is_empty())
+
+    def test_single_item_push_pop_value(self):
+        value = random.randint(-1000000, 1000000)
+        self.s.push(value)
+        pop_value = self.s.pop()
+        self.assertEqual(value, pop_value)
+
+    def test_multi_item_push_pop_non_empty(self):
+        for i in range(0, random.randint(10,100)):
+            self.s.push(random.randint(-1000000, 1000000))
+        self.assertFalse(self.s.is_empty())
+
+    def test_multi_item_push_pop_empty(self):
+        for i in range(0, 1000):
+            self.s.push(random.randint(-1000000, 1000000))
+        for i in range(0, 1000):
+            self.s.pop()
+        self.assertTrue(self.s.is_empty())
+
+    def test_multi_item_push_pop_value(self):
+        for i in range(-100, 100):
+            self.s.push(i)
+        for i in range(99, -101, -1):
+            self.assertEqual(self.s.pop(), i)
+
+
+if __name__ == 'main':
+    unittest.main()
