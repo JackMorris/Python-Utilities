73862e25bb7b238a1954c79f93db8daaf7dcc8ac
Jack Morris <jack.morris10@gmail.com>
2013-12-09 22:15:08 +0000

    Updated stack.py to provide better encapsulation.
    Stack class now has internal list as a 'private' member (prefixed with _)

--- DataStructures/stack.py
+++ DataStructures/stack.py
@@ -3,19 +3,19 @@ class Stack:
 
     def __init__(self):
         """ Initialises an empty stack. Complexity: O(1) """
-        self.data = []
+        self._data = []
 
     def is_empty(self):
         """ Returns True if the stack contains no elements. Complexity: O(1) """
-        return len(self.data) == 0
+        return len(self._data) == 0
 
     def push(self, x):
         """ Pushes an element x onto the stack. Complexity: O(1) """
-        self.data.append(x)
+        self._data.append(x)
 
     def pop(self):
         """ Returns and removes the element at the head of the stack. Raises IndexError if empty. Complexity: O(1) """
         if self.is_empty():
             raise IndexError("stack empty")
         else:
-            return self.data.pop()
+            return self._data.pop()
