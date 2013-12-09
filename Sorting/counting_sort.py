874cdb09eb4d8c050872efb636294e599f9b8140
Jack Morris <jack.morris10@gmail.com>
2013-12-09 19:39:51 +0000

    Fixed usage of min/max in counting_sort.
    Previously, counting_sort referenced variables called min and max, rather than min_value and max_value. Instead, they resolved to the builtin functions min and max, causing an error.

--- Sorting/counting_sort.py
+++ Sorting/counting_sort.py
@@ -11,15 +11,15 @@ def counting_sort(data):
     max_value = max(data)
     count_list = [0]*(max_value-min_value+1)
     for i in range(0, len(data)):
-        count_index = data[i]-min
+        count_index = data[i]-min_value
         count_list[count_index] += 1
 
-    count_value = max
+    count_value = max_value
     data_index = len(data) - 1
     while data_index >= 0:
-        if count_list[count_value-min] == 0:
+        if count_list[count_value-min_value] == 0:
             count_value -= 1
         else:
             data[data_index] = count_value
-            count_list[count_value-min] -= 1
+            count_list[count_value-min_value] -= 1
             data_index -= 1
