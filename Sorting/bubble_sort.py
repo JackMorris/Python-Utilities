a70ae9e03ee7ff969d5eb3b2dfbe0ca94e4d69f5
Jack Morris <jack.morris10@gmail.com>
2013-12-11 10:27:51 +0000

    Slightly improved readability and efficiency of Sorting/bubble_sort.
    - Bubble Sort now keeps track of which part of the list is already sorted (the end) and stops subsequent passes before it.
    - Variable name changes for improved clarity.

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -8,13 +8,14 @@ Space complexity: O(n) total, O(1) auxiliary (in place)
 
 
 def bubble_sort(data):
-    swap_made = True
-    while swap_made:
+    for pass_number in range(0, len(data)-1):
         swap_made = False
-        for i in range(0, len(data)-1):
+        for i in range(0, len(data)-pass_number-1):
             if data[i] > data[i+1]:
                 swap_made = True
                 # Swap data[i] and data[i+1]
                 tmp = data[i]
                 data[i] = data[i+1]
                 data[i+1] = tmp
+        if not swap_made:
+            break
